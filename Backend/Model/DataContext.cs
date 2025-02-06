78cfdde1bcd172865825b68bfcb724f9fa903a78
Leib92 <opilane@HP-CZC321705Q>
2025-02-06 14:05:23 +0200

    Added Event Data to DataContext

--- Backend/Model/DataContext.cs
+++ Backend/Model/DataContext.cs
@@ -4,6 +4,41 @@ namespace backend.Model;
 public class DataContext: DbContext {
     public DataContext(DbContextOptions<DataContext> options): base(options) {}
     public DbSet<Event>? EventList {get; set;}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Event>().Property(p => p.Id).HasIdentityOptions(startValue: 4);
+
+        modelBuilder.Entity<Event>().HasData(
+            new Event
+            {
+                Id = 1,
+                Type = "Hiveworld",
+                Description = "Trillions denizens live here.",
+                Location = "Seguntum I",
+                Date = new DateTime()
+            },
+            new Event
+            {
+                Id = 2,
+                Type = "Agriworld",
+                Description = "Starch for the masses.",
+                Location = "Valtrox VII",
+                Date = new DateTime()
+            },
+            new Event
+            {
+                Id = 3,
+                Type = "Deathworld",
+                Description = "WHY ARE YOU HERE!?",
+                Location = "Eye of Terror",
+                Date = new DateTime()
+            }
+            );
+
+    }
 }
 
 
